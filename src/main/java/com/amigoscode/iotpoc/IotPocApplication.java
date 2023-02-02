6b9e6e5f20a4e8f50406256b849b4097c3d9a695
rafal-nowak <contact@rafal-nowak.com>
2023-02-02 12:57:05 +0100

    initial commit

---
+++ src/main/java/com/amigoscode/iotpoc/IotPocApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode.iotpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotPocApplication.class, args);
+    }
+
+}
